f483bed228ea00514a65704890b92f7fd79ab9bb
Quang Cao <quangcdse140367@fpt.edu.vn>
2021-10-22 21:38:29 +0700

    create products page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_21_134425) do
+ActiveRecord::Schema.define(version: 2021_10_22_135046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,8 @@ ActiveRecord::Schema.define(version: 2021_10_21_134425) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "category_id", null: false
+    t.integer "status", default: 0
+    t.integer "quantity", default: 0
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
