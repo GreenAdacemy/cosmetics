92020ca20b132b8989f49af66f7166be012b2781
Phuong Ngo <ngohoai.phuong@gmail.com>
2021-10-29 18:35:34 +0700

    fix issue not wrong run hotwire when update

--- app/models/product.rb
+++ app/models/product.rb
@@ -38,7 +38,12 @@ class Product < ApplicationRecord
 
   after_create_commit { broadcast_prepend_to "products" }
   after_destroy_commit { broadcast_remove_to "products" }
-  after_update_commit { broadcast_replace_to "products" }
+  after_update_commit do
+    broadcast_replace_to :show, 
+      partial: 'products/item',
+      locals: { item: self },
+      target: "show-product-#{id}"
+  end
 
   def attach_url(index = 0)
     return nil unless self.images.attached?
